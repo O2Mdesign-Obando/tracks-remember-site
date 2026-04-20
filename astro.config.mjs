32641e7e73f63b8b675b5cec39ace5df49227da6
Jorge Obando <jorgerobando@hotmail.com>
2026-04-20 07:53:51 -0400

    Add robots.txt and sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://thetracksremember.com'
+  site: 'https://www.thetracksremember.com',
+  integrations: [sitemap()]
 });
