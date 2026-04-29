b93a1382338b9df152e7b2311bcf320677b908d9
O2Mdesign-Obando <jorgerobando@hotmail.com>
2026-04-29 14:25:25 -0400

    Alt tag updates and addition of What Remains

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://www.thetracksremember.com',
-  integrations: [sitemap()]
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  integrations: [sitemap()],
 });
